0d1f8a5192ebd94cb5dddc0fea5122cc26d3eb81
buu.nguyen <buu.nguyen@cake.vn>
2026-04-01 15:48:07 +0700

    add aliases

---
+++ .zshrc.d/aliases-cake.zsh
@@ -0,0 +1,10 @@
+## Gcloud alias
+alias gauth="gcloud auth login --update-adc"
+alias gke-prod="gcloud container clusters get-credentials cake-prod-1 --region asia-southeast1 --project bef-cake-prod"
+alias gke-platform-non-prod="gcloud container clusters get-credentials platform-as-se1-non-prod --region asia-southeast1 --project platform-as-se1-non-prod"
+alias gke-platform-prod="gcloud container clusters get-credentials platform-as-se1-prod --region asia-southeast1 --project platform-as-se1-prod"
+alias gke-config-controller="gcloud container clusters get-credentials config-controller-prod --region asia-southeast1 --project platform-as-se1-prod"
+
+# Check bigquery-access-control
+alias check-bac="gcloud container clusters get-credentials config-controller-prod --region asia-southeast1 --project platform-as-se1-prod && kubectl get bigquerydatasets,gcpiampartialpolicies -n cake-bigquery-admin -o json | jq '.items[] | select(.status.conditions[] | select(.type==\"Ready\" and .status!=\"True\")) | {name: .metadata.name, status: (.status.conditions[] | select(.type==\"Ready\") | .status), reason: (.status.conditions[] | select(.type==\"Ready\") | .reason), message: (.status.conditions[] | select(.type==\"Ready\") | .message)}'"
+alias homelab="kubectl config use-context admin@homelab"
--- .zshrc.d/aliases.zsh
+++ .zshrc.d/aliases.zsh
@@ -39,3 +39,6 @@ alias please=sudo
 alias zshrc='${EDITOR:-vim} "${ZDOTDIR:-$HOME}"/.zshrc'
 alias zbench='for i in {1..10}; do /usr/bin/time zsh -lic exit; done'
 alias zdot='cd ${ZDOTDIR:-~}'
+
+# nvim
+alias vim="nvim"
