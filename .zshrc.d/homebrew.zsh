631c52574fdd6e448025c5501116691120988387
blue <blue@thealtoclef.com>
2026-04-15 00:18:11 +0700

    Update zshrc.d

--- .zshrc.d/homebrew.zsh
+++ .zshrc.d/homebrew.zsh
@@ -1,7 +1,15 @@
+# Detect OS and set Homebrew root path accordingly
 case $(uname) in
-Linux)  _brew_path="/home/linuxbrew/.linuxbrew/bin/brew" ;;
-Darwin) _brew_path="/opt/homebrew/bin/brew" ;;
+Linux)  _brew_root_path="/home/linuxbrew/.linuxbrew" ;;
+Darwin) _brew_root_path="/opt/homebrew" ;;
 esac
 
-[[ -x $_brew_path ]] && eval "$($_brew_path shellenv)"
-unset _brew_path
+# Initialize brew shellenv if the executable exists
+_brew_bin_path="$_brew_root_path/bin/brew"
+[[ -x $_brew_bin_path ]] && eval "$($_brew_bin_path shellenv)"
+
+# Add Google Cloud SDK bin to PATH
+export PATH="$_brew_root_path/share/google-cloud-sdk/bin:$PATH"
+
+# Clean up variables
+unset _brew_root_path _brew_bin_path
