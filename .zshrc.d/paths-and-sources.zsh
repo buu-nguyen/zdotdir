f8ea9c19ace410aad138b6258f4e9c3e9d147dd7
blue <blue@thealtoclef.com>
2026-06-20 15:59:23 +0700

    update

--- .zshrc.d/paths-and-sources.zsh
+++ .zshrc.d/paths-and-sources.zsh
@@ -1,9 +1,14 @@
 export BUN_INSTALL="$HOME/.bun"
 export SDKMAN_DIR="$HOME/.sdkman"
 
+PATHS=(
+    "$BUN_INSTALL/bin"
+)
+
+export PATH="$(IFS=:; echo "${PATHS[*]}"):$PATH"
+
 SOURCES=(
     "$HOME/.cargo/env"
-    "$BUN_INSTALL/bin"
     "$BUN_INSTALL/_bun"
     "$SDKMAN_DIR/bin/sdkman-init.sh"
 )
