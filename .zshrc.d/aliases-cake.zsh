9a5f8be1fc80ea633713bc865737d9ece5f46633
buu.nguyen <buu.nguyen@cake.vn>
2026-08-04 13:05:19 +0700

    update

--- .zshrc.d/aliases-cake.zsh
+++ .zshrc.d/aliases-cake.zsh
@@ -6,4 +6,4 @@ alias gke-platform-non-prod="gcloud container clusters get-credentials platform-
 alias check-bac="kubectl get bigquerydatasets,gcpiampartialpolicies --context=gke_platform-as-se1-prod_asia-southeast1_config-controller-prod -n cake-bigquery-admin -o json | jq '.items[] | select(.status.conditions[] | select(.type==\"Ready\" and .status!=\"True\")) | {name: .metadata.name, status: (.status.conditions[] | select(.type==\"Ready\") | .status), reason: (.status.conditions[] | select(.type==\"Ready\") | .reason), message: (.status.conditions[] | select(.type==\"Ready\") | .message)}'"
 
 # Telepresence
-alias tlp="telepresence connect --context gke_cake-as-se1-dev_asia-southeast1_cake-as-se1-dev"
+alias tlp="telepresence quit && telepresence connect --context gke_cake-as-se1-dev_asia-southeast1_cake-as-se1-dev"
